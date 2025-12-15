99af4842ae36f772da37354839c7fdc6abca5162
Joakim Englund <joakimenglund@protonmail.com>
2025-12-15 14:26:46 +0100

    Added two teachers

--- src/main/java/se/chasacademy/databaser/jpastart/JPAStartApplication.java
+++ src/main/java/se/chasacademy/databaser/jpastart/JPAStartApplication.java
@@ -3,17 +3,24 @@ package se.chasacademy.databaser.jpastart;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.chasacademy.databaser.jpastart.Models.Teacher;
+import se.chasacademy.databaser.jpastart.Repositories.TeacherRepository;
 
 @SpringBootApplication
 public class JPAStartApplication implements CommandLineRunner {
+    private TeacherRepository teacherRepository;
 
+    JPAStartApplication(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JPAStartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JPAStartApplication.class, args);
+    }
 
 	@Override
 	public void run(String... args) {
-
+        teacherRepository.save(new Teacher("adam.bertilsson@skolmail.se", "Adam Bertilsson"));
+        teacherRepository.save(new Teacher("caesar.davidsson@skolmail.se", "Caesar Davidsson"));
 	}
 }

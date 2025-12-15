2f9a1300a199f2317dcb690937804f7f0ef1755f
Svartakatten <rikexhx@gmail.com>
2025-12-15 13:35:09 +0100

    added Student Class

---
+++ src/main/java/se/chasacademy/databaser/jpastart/Models/Course.java
@@ -0,0 +1,72 @@
+package se.chasacademy.databaser.jpastart.Models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(unique = true)
+    private String courseCode;
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students = new ArrayList<>();
+
+    public Course() {}
+
+    public Course(String title, String courseCode, Teacher teacher) {
+        this.title = title;
+        this.courseCode = courseCode;
+        this.teacher = teacher;
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+        student.getCourses().add(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
